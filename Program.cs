5a3e3520c0570f34a9b4cd009f70b2b591ce9663
syahiratuzzulfa <85855859+syahiratuzzulfa@users.noreply.github.com>
2022-03-02 15:51:37 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ACER
+ * Date: 02/03/2022
+ * Time: 14:53
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace Tugas4PV
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
