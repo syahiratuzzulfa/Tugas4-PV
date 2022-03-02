ccd1bb8de892cfc1c762b6b1b091f3a0db482bf2
syahiratuzzulfa <85855859+syahiratuzzulfa@users.noreply.github.com>
2022-03-02 15:52:29 +0700

    Add files via upload

---
+++ MainForm.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ACER
+ * Date: 02/03/2022
+ * Time: 14:53
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Tugas4PV
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void TextBox1TextChanged(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+	      MessageBox.Show (" Terimakasih " + this.textBox1.Text);
+	      
+		}
+	}
+}
